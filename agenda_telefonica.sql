e119440769578ea0ff5592419325ffebe36ad47f
Kleber Marreiro <kleber.marreiro@hotmail.com>
2024-08-22 11:36:17 -0300

    adição dos comentarios aos comandos feitos no PostgreSQL para criação do banco de dados e da tabela contatos

--- agenda_telefonica.sql
+++ agenda_telefonica.sql
@@ -1,8 +1,13 @@
+-- Comando usado no PostgreSQL para criar o Banco de Dados
+CREATE DATABASE agenda_telefonica;
+
+-- Comando usado no PostgreSQL para criar a tabela Contatos
 CREATE TABLE contatos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100),
     telefone VARCHAR(15)
 );
 
+-- Comando usado para consultar a tabela Contatos
 SELECT * FROM contatos;
 

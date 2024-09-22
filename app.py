82564aca8933800b5278624439d84c9f21185115
Kleber Marreiro <kleber.marreiro@hotmail.com>
2024-09-22 15:55:22 -0300

    Inclusão do Evento de Buscar Contatos usando o ILIKE para consultas insencível a maiúsculas /minúsculas

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import psycopg2
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Esta linha permite requisições de qualquer origem #
+CORS(app, resources={r"/*": {"origins": "*"}})  # Permite requisições de qualquer origem
 
-# Configurações do banco de dados #
+# Configurações do banco de dados
 conn = psycopg2.connect(
     host="localhost",
     database="agenda_telefonica",
@@ -21,7 +21,7 @@ def add_contact():
         telefone = data['telefone']
 
         cursor = conn.cursor()
-        # Verifica se o contato já existe #
+        # Verifica se o contato já existe
         cursor.execute("SELECT COUNT(*) FROM contatos WHERE nome = %s AND telefone = %s", (nome, telefone))
         count = cursor.fetchone()[0]
 
@@ -37,6 +37,28 @@ def add_contact():
 
     except Exception as e:
         return jsonify({"message": f"Erro ao salvar contato: {e}"}), 500
+
+
+@app.route('/search', methods=['POST'])
+def search_contact():
+    try:
+        data = request.get_json()
+        search_value = data['search']
+
+        cursor = conn.cursor()
+        # Busca por nome ou telefone com ILIKE para busca insensível a maiúsculas/minúsculas
+        cursor.execute("SELECT nome, telefone FROM contatos WHERE nome ILIKE %s OR telefone ILIKE %s", (f'%{search_value}%', f'%{search_value}%'))
+        contact = cursor.fetchone()
+        cursor.close()
+
+        if contact:
+            return jsonify({"nome": contact[0], "telefone": contact[1]})
+        else:
+            return jsonify({"error": "Contato não encontrado!"}), 404
+
+    except Exception as e:
+        return jsonify({"error": f"Erro ao procurar contato: {e}"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)

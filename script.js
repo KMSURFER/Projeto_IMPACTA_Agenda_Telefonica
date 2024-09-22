9c201de42fddab668c52199794a3cfc9995c675d
Kleber Marreiro <kleber.marreiro@hotmail.com>
2024-09-22 15:59:57 -0300

    Inclusão do Campo de busca para buscar contato e Limpeza do campo de busca

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Evento de adicionar contato
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -19,4 +20,43 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
         document.getElementById('telefone').value = '';
     })
     .catch(error => console.error('Erro:', error));
+});
+
+// Mostrar campo de busca ao clicar no botão "Procurar Contato"
+document.getElementById('searchButton').addEventListener('click', function() {
+    document.getElementById('searchDiv').style.display = 'block';
+});
+
+// Evento de busca
+document.getElementById('searchContactButton').addEventListener('click', function() {
+    let searchValue = document.getElementById('searchInput').value;
+
+    fetch('http://127.0.0.1:5000/search', {
+        method: 'POST',  // Assegure que o método é POST
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ search: searchValue })
+    })
+    .then(response => response.json())
+    .then(data => {
+        let searchResultDiv = document.getElementById('searchResult');
+        if (data.error) {
+            searchResultDiv.textContent = data.error;
+        } else {
+            searchResultDiv.innerHTML = `
+                <p><strong>Nome:</strong> ${data.nome}</p>
+                <p><strong>Telefone:</strong> ${data.telefone}</p>
+                <p>Contato encontrado com sucesso!</p>
+            `;
+        }
+    })
+    .catch(error => console.error('Erro:', error));
+});
+
+// Evento de limpar busca
+document.getElementById('clearSearchButton').addEventListener('click', function() {
+    // Limpar o campo de busca e o resultado
+    document.getElementById('searchInput').value = '';
+    document.getElementById('searchResult').innerHTML = '';
 });
